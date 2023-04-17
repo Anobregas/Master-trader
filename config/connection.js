5b8876e123d0d1d34556b4ee7d132082d5b9cd74
Austin Nobregas <austinnobregas@gmail.com>
2023-04-16 23:38:28 -0400

    bug

--- config/connection.js
+++ config/connection.js
@@ -20,16 +20,16 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306,
-//   }
-// );
+
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
+  // {
+  //   host: 'localhost',
+  //   dialect: 'mysql',
+  //   port: 3306,
+  // }
+
 
 module.exports = sequelize;
 
